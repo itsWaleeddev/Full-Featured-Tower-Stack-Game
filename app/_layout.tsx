c471e04d92b1a71e520dcbd428c9d9d5b30bad3a
itsWaleeddev <link2waleed01@gmail.com>
2025-08-21 13:18:48 +0500

    Updated async version and _layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout() {
       <SoundProvider>
         <ThemeProvider>
           <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="+not-found" />
           </Stack>
           <StatusBar style="auto" />
